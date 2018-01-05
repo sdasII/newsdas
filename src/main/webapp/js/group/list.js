e6d249c66691dbc5150943f6bbf26d54142413d7
liuyan <2841955648@qq.com>
2018-01-05 11:09:59 +0800

    修改小区页面的预警列表和健康评估页面列表父子级形式

--- src/main/webapp/js/group/list.js
+++ src/main/webapp/js/group/list.js
@@ -3,6 +3,7 @@
  */
 var cellListUrl = ctx + '/alarm/celllist';
 var updatetimeUrl = ctx + "/alarm/updatetime"
+var cellDetailUrl=ctx+"/alarm/celllist/incell";
 /*
  * 全局查询字段
  */
@@ -33,18 +34,19 @@ $(function(){
         pageNumber : 1,  
         pageList : [ 10, 20, 30 ],
         clickToSelect : true,
+        detailView: true,//父子表
         sidePagination : 'server',// 设置为服务器端分页
         columns : [
-            { field : "cellname", title : "小区名称", align : "center", valign : "middle",
+            { field : "cell_code", title : "小区名称", align : "center", valign : "middle",
             	formatter:function(value,row,index){
                     var url = ctx + "/alarm/todetail";
                     var params = "[{\"key\":\"cell_code\",\"value\":\""+value+"\"}]";
                     var url = '<a href=javascript:iframeconvert("' + url + '","小区日常监控",' + params + ')>'+value+'</a>';
                     return url;
               }},
-            { field : "yyyymmdd", title : "日期", align : "center", valign : "middle"},
-            { field : "hour", title : "时间", align : "center", valign : "middle"},
-            { field : "result", title : "状态", align : "center", valign : "middle",
+            { field : "yyyyMMdd", title : "日期", align : "center", valign : "middle"},
+            /*{ field : "hour", title : "时间", align : "center", valign : "middle"},*/
+            /*{ field : "result", title : "状态", align : "center", valign : "middle",
             	formatter:function(value,row,index){
                    var str="";
                    if(value==0){
@@ -57,8 +59,8 @@ $(function(){
                 	   str="计算无结果"
                    }
                     return str;
-              }},
-            { field : "calcultime", title : "计算时间", align : "center", valign : "middle",
+              }},*/
+            { field : "create_time", title : "计算时间", align : "center", valign : "middle",
             	  formatter:function(value,row,index){
 	                  var jsDate = new Date(value);
 	                  var UnixTimeToDate = jsDate.getFullYear() + '/' + (jsDate.getMonth() + 1) + '/'+jsDate.getDate()+ ' ' + jsDate.getHours() + ':' + jsDate.getMinutes() + ':' + jsDate.getSeconds();
@@ -66,6 +68,9 @@ $(function(){
 	                 }
               }            
         ],
+        onExpandRow: function (index, row, $detail) {
+        	detail_table(index, row, $detail);
+        },
         onPageChange : function(size, number) {
             searchInfo();
         },
@@ -93,6 +98,55 @@ $(function(){
 });
 //全局查询参数
 var bsdata = {};
+//初始化子表格
+function  detail_table(index, row, $detail){
+    var cur_table = $detail.html('<table></table>').find('table');
+    $(cur_table).bootstrapTable({
+        url: cellDetailUrl,
+        method: 'get',
+        queryParams: {
+        	"name": row.cell_code,
+        	"yyyymmdd": row.yyyymmdd,
+        	"type":$("#status").val()
+        },
+        clickToSelect: true,
+        detailView: true,//父子表
+        pageSize: 10,
+        pageList: [10, 25],
+        columns : [
+                   { field : "cell_code", title : "小区名称", align : "center", valign : "middle",
+                   	formatter:function(value,row,index){
+                           var url = ctx + "/alarm/todetail";
+                           var params = "[{\"key\":\"cell_code\",\"value\":\""+value+"\"}]";
+                           var url = '<a href=javascript:iframeconvert("' + url + '","小区日常监控",' + params + ')>'+value+'</a>';
+                           return url;
+                     }},
+                   /*{ field : "yyyyMMdd", title : "日期", align : "center", valign : "middle"},*/
+                   { field : "hour", title : "时间", align : "center", valign : "middle"},
+                   { field : "result", title : "状态", align : "center", valign : "middle",
+                   	formatter:function(value,row,index){
+                          var str="";
+                          if(value==0){
+                       	   str="事件"
+                          }else if(value==1){
+                       	   str="亚健康"
+                          }else if(value==2){
+                       	   str="健康"
+                          }else if(value==3){
+                       	   str="计算无结果"
+                          }
+                           return str;
+                     }},
+                   { field : "create_time", title : "发布时间", align : "center", valign : "middle",
+                   	  formatter:function(value,row,index){
+       	                  var jsDate = new Date(value);
+       	                  var UnixTimeToDate = jsDate.getFullYear() + '/' + (jsDate.getMonth() + 1) + '/'+jsDate.getDate()+ ' ' + jsDate.getHours() + ':' + jsDate.getMinutes() + ':' + jsDate.getSeconds();
+       	                   return UnixTimeToDate;
+       	                 }
+                     }            
+               ],
+    });
+}
 // 查询表格信息
 function searchInfo() { 
 	bsdata.cellname=$("#name").val();
