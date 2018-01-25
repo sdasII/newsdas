4193df25581ff53d258dff219b502727fdf01583
junwei <junwei@nfs-qd.com>
2018-01-25 19:35:36 +0800

    upate

--- src/main/java/com/iscas/sdas/controller/data/DataController.java
+++ src/main/java/com/iscas/sdas/controller/data/DataController.java
@@ -225,10 +225,7 @@ public class DataController{
 		String password=PropertyUtil.getValueByKey("config.properties","ftp_connect_password");
 		//
 		try {
-            myFtp.connect(hostname, port, username, password);//cmcc
-            //myFtp.connect("188.1.31.41", 21, "hadoop", "nfs_qd123");//cmcc
-            //myFtp.connect("192.168.9.70", 21, "hadoop", "nfs_qd123");//nfs
-			//myFtp.connect("49.4.6.146", 21, "hadoop", "nfs_qd123");//huawei
+            myFtp.connect(hostname, port, username, password);//huawei
 			
 			System.out.println("3...连接到ftp");
 			request.getSession().setAttribute(Constraints.SESSION_FTP_STATUS, myFtp);						
